d05e8354b2504cdfca54d639e88eb5beafa0943b
furenku <furenku@gmail.com>
2019-04-07 01:21:24 -0600

    change fonts links

--- index.js
+++ index.js
@@ -73,14 +73,14 @@ app.get('/', (req, res) => {
             font-family: 'Suisse';
             font-style: normal;
             font-weight: normal;
-            src: url('http://192.241.194.153:8080/fonts/SuisseIntl-Regular.db0a4a5d.otf');
+            src: url('http://192.241.194.153:8080/fonts/SuisseIntl-Regular.otf');
         }
 
         @font-face {
             font-family: 'Suisse';
             font-style: bold;
             font-weight: 800;
-            src: url('http://192.241.194.153:8080/fonts/SuisseIntl-Medium.db0a4a5d.otf');
+            src: url('http://192.241.194.153:8080/fonts/SuisseIntl-Medium.otf');
         }
 
 
